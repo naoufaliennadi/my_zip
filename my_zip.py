e182b5ba2ae8ba31d6305ceb1654d889b059c839
Naoufal <naoufal.enna@gmail.com>
2024-10-11 04:15:50 -0500

    finished implementation

--- my_zip.py
+++ my_zip.py
@@ -1,8 +1,20 @@
 # my_zip.py
 def my_zip(*args):
-    # list to store iterables
-    # tryl = []
-    # make all iterables
-    # for i in args:
-    # tryl.append(iter(i))
-    return []
+    '''
+        zip function written in python
+        takes in any number of non indexable iterables
+        with the same length
+        returns a list of tuples, where each tuple contains
+        the i-th element
+    '''
+    # turn every iterable into an iterator
+    itrs = [iter(i) for i in args]
+    # if any iterators were made
+    if len(itrs):
+        while True:
+            # return the i-th element
+            try:
+                yield tuple([next(i) for i in itrs])
+            # stop if the end of any iterator was reached
+            except StopIteration:
+                break
